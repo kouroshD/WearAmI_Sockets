ec04b7abf71847df37454b1e3888dc7bd60e1b7f
Kourosh <kourosh.darvish@gmail.com>
2017-05-05 19:28:48 +0200

    small modification on rospysserver offline

--- src/ros_server_offline.py
+++ src/ros_server_offline.py
@@ -23,27 +23,40 @@ def main():
 	r = rospy.Rate(40) # period
 	signal.signal(signal.SIGINT, signal_handler)
 	
-	filePath = os.path.join('/home/nasa/Datalog/ICRA_TESTS',"01_Acc_data"+".txt")
+	filePath = os.path.join('/home/nasa/Datalog/ICRA_TESTS',"59_Acc_data"+".txt")
 	inputFile = open(filePath, "r")
 	pub = rospy.Publisher('wearami_acc', Pose, queue_size=10)
 
 	counterROS=0
+	completeName = os.path.join('/home/nasa/Datalog/ICRA_TESTS',"200_Acc_data"+".txt")
+	file = open(completeName, "a")
 
 # 	while not rospy.is_shutdown() and
+	secondsInit = rospy.get_time()
 	for line in inputFile:
 		counterROS=counterROS+1 
 		cleanLine = line.rstrip(' \n')
 		Acc_list = [float(x) for x in cleanLine.split(' ')][0:4]
 		assert len(Acc_list)> 0
-# 		print counterROS,': ',Acc_list[1],' ', Acc_list[2],' ', Acc_list[3]
+#  		print counterROS,': ',Acc_list[0],' ', Acc_list[1],' ', Acc_list[2],' ', Acc_list[2]
 		accel = Pose()
 		accel.position.x=Acc_list[1]
 		accel.position.y=Acc_list[2]
 		accel.position.z=Acc_list[3]
 		pub.publish(accel)
-		r.sleep()
 		
-
+		micro_sec = int(round(time.time() * 1e6))
+		#print micro_sec
+		file.write('{} {} {} {}'.format(str(micro_sec),float(Acc_list[1]),float(Acc_list[1]),float(Acc_list[1])))
+		file.write("\n")
+		if counterROS>1:
+			time_now=Acc_list[0]
+			elapsed_time=(time_now-time_prev)/1000000
+			rospy.sleep(elapsed_time)
+		time_prev=Acc_list[0]
+	secondsEnd = rospy.get_time()
+	total_time=secondsEnd-secondsInit
+	print 'total_time: ',total_time
 ###############################################
 
 if __name__ == '__main__':
